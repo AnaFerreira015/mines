f18f81d9d9bee3507e5bd809878a1ed9c87b4dd4
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 22:54:34 -0300

    Defines style according to the number of mines

--- src/components/Field.js
+++ src/components/Field.js
@@ -6,7 +6,16 @@ export default props => {
     const { mined, opened, nearMines } = props
 
     const styleField = [styles.field]
+    if (opened) styleField.push(styles.opened)
     if (styleField.length === 1) styleField.push(styles.regular)
+
+    let color = null
+    if (nearMines > 0) {
+        if (nearMines == 1) color = "#2A28D7"
+        if (nearMines == 2) color = "#2B520F"
+        if (nearMines > 2 && nearMines < 6) color = "#F9060A"
+        if (nearMines >= 6) color = "#F221A9"
+    }
 
     return (
         <View style={styleField}>
