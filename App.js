df6734cdbebb51d5bf498b2bd9395b98f2d7f037
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 23:41:37 -0300

    Create Flag component

--- App.js
+++ App.js
@@ -23,6 +23,8 @@ export default class App extends Component {
           <Field mined />
           <Field mined opened />
           <Field mined opened exploded />
+          <Field flagged />
+          <Field flagged opened />
       </View>
     )
   }
