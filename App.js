12796ba4b9ae780fd82712a2d6389801c5370a9e
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-16 00:50:34 -0300

    Create and use MineField component

--- App.js
+++ App.js
@@ -5,26 +5,39 @@ import {
   Text,
 } from 'react-native';
 import params from "./src/params"
-import Field from "./src/components/Field"
+import MineField from "./src/components/MineField"
+import { createMinedBoard } from "./src/functions"
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instrucions}>Tamanho da grade: 
             {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
-          <Field />
-          <Field opened />
-          <Field opened nearMines={1} />
-          <Field opened nearMines={2} />
-          <Field opened nearMines={3} />
-          <Field opened nearMines={6} />
-          <Field mined />
-          <Field mined opened />
-          <Field mined opened exploded />
-          <Field flagged />
-          <Field flagged opened />
+        
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </View>
     )
   }
@@ -33,19 +46,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    // justifyContent: "space-around",
+    justifyContent: "flex-end",
+  },
+  board: {
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10,
-  },
-  instrucions: {
-    fontSize: 15,
-    textAlign: "center",
-    margin: 10,
-  },
+    backgroundColor: "#AAA",
+  }
 });
