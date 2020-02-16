b7b908eb8ffc09e5895682f7e4b8dc3960b2b4ba
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 22:28:46 -0300

    Create field component for the game

---
+++ src/components/Field.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import params from "../params"
+
+export default props => {
+    const styleField = [styles.field]
+    if (styleField.length === 1) styleField.push(styles.regular)
+
+    return (
+        <View style={styleField}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create ({
+    field: {
+        height: params.blockSize,
+        width: params.blockSize,
+        borderWidth: params.borderSize,
+    },
+    regular: {
+        backgroundColor: "#999",
+        borderLeftColor: "#CCC",
+        borderTopColor: "#CCC",
+        borderRightColor: "#333",
+        borderBottomColor: "#333",
+    }
+})
