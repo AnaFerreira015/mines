79e260a67120867be304a6f1ea194a44e5718cf3
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-16 00:53:54 -0300

    Fiz MineField

--- src/components/MineField.js
+++ src/components/MineField.js
@@ -7,14 +7,15 @@ export default props => {
         const columns = row.map((field, c) => {
             return <Field {...field} key={c} />
         })
-        return <View key={r}>{columns}</View>
+        return <View key={r}
+            style={{flexDirection: "row"}}>{columns}</View>
     })
     return <View style={styles.container}>{rows}</View>
 }
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: "row",
+        // flexDirection: "row",
         backgroundColor: "#EEE",
     }
 })
