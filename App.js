365ab71d1e5ab59aca7e1cd6690559b59d95d27a
anaferreira015 <dasilvaferreira.af@hotmail.com>
2020-02-15 23:21:53 -0300

    Finalizes Mine component

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   View,
   Text,
 } from 'react-native';
 import params from "./src/params"
+import Field from "./src/components/Field"
 
 export default class App extends Component {
   render () {
@@ -14,6 +14,15 @@ export default class App extends Component {
         <Text style={styles.welcome}>Iniciando o Mines!</Text>
         <Text style={styles.instrucions}>Tamanho da grade: 
             {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+          <Field />
+          <Field opened />
+          <Field opened nearMines={1} />
+          <Field opened nearMines={2} />
+          <Field opened nearMines={3} />
+          <Field opened nearMines={6} />
+          <View mined />
+          <View mined opened />
+          <View mined opened exploded />
       </View>
     )
   }
--- src/components/Field.js
+++ src/components/Field.js
@@ -1,12 +1,14 @@
 import React from "react"
 import { View, StyleSheet, Text } from "react-native"
 import params from "../params"
+import Mine from "./Mine"
 
 export default props => {
-    const { mined, opened, nearMines } = props
+    const { mined, opened, nearMines, exploded } = props
 
     const styleField = [styles.field]
     if (opened) styleField.push(styles.opened)
+    if (exploded) styleField.push(styles.exploded)
     if (styleField.length === 1) styleField.push(styles.regular)
 
     let color = null
@@ -19,7 +21,11 @@ export default props => {
 
     return (
         <View style={styleField}>
+            {!mined && opened && nearMines > 0 ? 
+                <Text style={[styles.label, { color: color}]}>
+                    {nearMines}</Text> : false}
 
+            {mined && opened ? <Mine /> : false}
         </View>
     )
 }
@@ -36,5 +42,19 @@ const styles = StyleSheet.create ({
         borderTopColor: "#CCC",
         borderRightColor: "#333",
         borderBottomColor: "#333",
+    },
+    opened: {
+        backgroundColor: "#999",
+        borderColor: "#777",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    label: {
+        fontWeight: "bold",
+        fontSize: params.fontSize,
+    },
+    exploded: {
+        backgroundColor: "red",
+        borderColor: "red"
     }
 })
